9abccaabc99ca34fe2d58fc1c866cded4fd7874b
Islam <104109698+IslamYasser17@users.noreply.github.com>
2025-05-10 16:42:45 +0300

    create todo service

---
+++ src/TodoManagement.Application/Todos/TodoAppService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoManagement.Todo;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using TodoManagement.Entities;
+
+
+namespace TodoManagement.Todos;
+
+public class TodoAppService : CrudAppService<TodoManagement.Entities.Todo, TodoDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateTodoDto>, ITodoAppService
+{
+    public TodoAppService(Volo.Abp.Domain.Repositories.IRepository<TodoManagement.Entities.Todo, Guid> repository) : base(repository)
+    {
+    }
+
+}
