c199d4ffd23b001da8cedc6a151c1ef4525a4df9
Islam <104109698+IslamYasser17@users.noreply.github.com>
2025-05-10 17:02:57 +0300

    adjust due date in creeatu update dto

--- src/TodoManagement.Application.Contracts/Todo/CreateUpdateTodoDto.cs
+++ src/TodoManagement.Application.Contracts/Todo/CreateUpdateTodoDto.cs
@@ -13,6 +13,6 @@ public class CreateUpdateTodoDto
     public string Description { get; set; }
     public Status Status { get; set; }
     public Priority Priority { get; set; }
-    public DateTime DueDate { get; set; }
+    public DateTime ? DueDate { get; set; }
 
 }
