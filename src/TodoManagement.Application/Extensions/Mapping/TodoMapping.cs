5bc1b13aa2627e68d287bfd95614d14570c42ce8
Islam <104109698+IslamYasser17@users.noreply.github.com>
2025-05-10 17:10:01 +0300

    add todo mappings

---
+++ src/TodoManagement.Application/Extensions/Mapping/TodoMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoManagement.Todo;
+
+namespace TodoManagement.Extensions.Mapping;
+
+public static class TodoMapping
+{
+    public static void MapTodo(this TodoManagementApplicationAutoMapperProfile map)
+    {
+        map.CreateMap<CreateUpdateTodoDto,TodoDto>().ReverseMap();
+        map.CreateMap<TodoManagement.Entities.Todo, TodoDto>().ReverseMap();
+    }
+}
--- src/TodoManagement.Application/TodoManagementApplicationAutoMapperProfile.cs
+++ src/TodoManagement.Application/TodoManagementApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using TodoManagement.Extensions.Mapping;
 
 namespace TodoManagement;
 
@@ -6,8 +7,8 @@ public class TodoManagementApplicationAutoMapperProfile : Profile
 {
     public TodoManagementApplicationAutoMapperProfile()
     {
-        /* You can configure your AutoMapper mapping configuration here.
-         * Alternatively, you can split your mapping configurations
-         * into multiple profile classes for a better organization. */
+        //Mapping todo entity and dtos
+        this.MapTodo();
+       
     }
 }
