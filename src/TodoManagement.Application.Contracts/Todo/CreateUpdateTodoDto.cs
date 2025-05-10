c9395d48ee6159f79464c6a8a95a178147fc9abb
Islam <104109698+IslamYasser17@users.noreply.github.com>
2025-05-10 16:41:36 +0300

    add todo dtos

---
+++ src/TodoManagement.Application.Contracts/Todo/CreateUpdateTodoDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoManagement.Enums;
+
+namespace TodoManagement.Todo;
+
+public class CreateUpdateTodoDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Status Status { get; set; }
+    public Priority Priority { get; set; }
+    public DateTime DueDate { get; set; }
+
+}
---
+++ src/TodoManagement.Application.Contracts/Todo/TodoDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoManagement.Enums;
+using Volo.Abp.Application.Dtos;
+
+namespace TodoManagement.Todo;
+
+public class TodoDto : AuditedEntityDto<Guid>
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Status Status { get; set; }
+    public Priority Priority { get; set; }
+    public DateTime DueDate { get; set; }
+}
